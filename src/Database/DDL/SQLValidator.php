01d01504e40bca03e4cacddc9d8b3aef56f30cb2
PeterSpainOXIL <114655461+PeterSpainOXIL@users.noreply.github.com>
2024-09-25 11:41:19 +0100

    temp comment out

--- src/Database/DDL/SQLValidator.php
+++ src/Database/DDL/SQLValidator.php
@@ -11,9 +11,9 @@ class SQLValidator {
         if (!mb_check_encoding($indexName, 'ASCII')){
             throw new InvalidIndexNameException("Cannot use non-ASCII characters in a index name.");
         }
-        if (!preg_match("/^[a-zA-Z0-9_]*$/", $indexName)){
-            throw new InvalidIndexNameException("Only alphanumeric characters and underscores are allowed in a index name.");
-        }
+//        if (!preg_match("/^[a-zA-Z0-9_]*$/", $indexName)){
+//            throw new InvalidIndexNameException("Only alphanumeric characters and underscores are allowed in a index name.");
+//        }
         if (preg_match("/^[0-9_]*$/", $indexName)){
             throw new InvalidIndexNameException("Index name cannot be all numbers.");
         }
@@ -25,9 +25,9 @@ class SQLValidator {
         if (!mb_check_encoding($tableName, 'ASCII')){
             throw new InvalidIndexNameException("Cannot use non-ASCII characters in a table name.");
         }
-        if (!preg_match("/^[a-zA-Z0-9_]*$/", $tableName)){
-            throw new InvalidTableNameException("Only alphanumeric characters and underscores are allowed in a table name.");
-        }
+//        if (!preg_match("/^[a-zA-Z0-9_]*$/", $tableName)){
+//            throw new InvalidTableNameException("Only alphanumeric characters and underscores are allowed in a table name.");
+//        }
         if (preg_match("/^[0-9_]*$/", $tableName)){
             throw new InvalidTableNameException("Table name cannot be all numbers.");
         }
